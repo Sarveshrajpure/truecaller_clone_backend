b3852830306abbfb26b5020f09347e3fe380a501
sarvesh rajpure <rajpuresarvesh@gmail.com>
2024-07-08 02:34:26 +0530

    fixed db config file

--- config/config.js
+++ config/config.js
@@ -8,6 +8,14 @@ module.exports = {
     host: process.env.DB_HOST,
     port: 5432,
     dialect: "postgres",
+  },
+  developmentCloudDB: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: 5432,
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false,
